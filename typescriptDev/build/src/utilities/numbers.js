716f86ea01603907f8cc29b782003fdea42aef5a
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-06-16 21:20:18 -0500

    add deploy to local dev and production to typscript dev

---
+++ typescriptDev/build/src/utilities/numbers.js
@@ -0,0 +1,44 @@
+"use strict";
+// multiply
+Object.defineProperty(exports, "__esModule", { value: true });
+// const multiply = (num1, num2) => {
+//   return num1 * num2;
+// };
+var multiply = function (num1, num2) {
+    return num1 * num2;
+};
+// add
+// const sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+var sum = function (num1, num2) {
+    return num1 + num2;
+};
+// divide
+// const divide = (num1, num2) => {
+//   return num1 / num2;
+// };
+var divide = function (num1, num2) {
+    return num1 / num2;
+};
+// subtract
+// const subtract = (num1, num2) => {
+//   return num1 - num2;
+// };
+var subtract = function (num1, num2) {
+    return num1 - num2;
+};
+// square
+// const square = (num) => {
+//   return num * num;
+// };
+var square = function (num) {
+    return num * num;
+};
+exports.default = {
+    multiply: multiply,
+    subtract: subtract,
+    divide: divide,
+    sum: sum,
+    square: square,
+};
