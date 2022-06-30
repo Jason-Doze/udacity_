dd9217af30e115f1e66762fc5934a09b1f79ae6f
Jason <90075907+Jason-Doze@users.noreply.github.com>
2022-06-29 21:52:54 -0500

    add middleware project to repo

---
+++ loggingMiddleWare/build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const logger_1 = __importDefault(require("./utilities/logger"));
+const app = (0, express_1.default)();
+const port = 3000;
+// routes
+app.get("./continents", logger_1.default, (req, res) => {
+    res.send("continents");
+});
+app.get("./countries", logger_1.default, (reg, res) => {
+    res.send("countries");
+});
+app.get("./oceans", logger_1.default, (req, res) => {
+    res.send("oceans");
+});
+// start the Express server
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
